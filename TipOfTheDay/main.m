77abcbdc436c72ce2d36c98bf9e82cdcb021606e
Tara Cates <tcates@apple.com>
2017-02-08 20:15:25 -0800

    Initial Commit

---
+++ TipOfTheDay/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TipOfTheDay
+//
+//  Created by Tara Cates on 2/8/17.
+//  Copyright © 2017 Tara Cates. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
